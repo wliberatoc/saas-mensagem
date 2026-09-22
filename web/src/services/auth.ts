ec88b124435fa4cc1d252e7685c3890ce9690f0b
Willian Castro <willian.castro@nerdweb.com.br>
2026-09-22 10:36:21 -0300

    criando auth.ts

--- web/src/services/auth.ts
+++ web/src/services/auth.ts
@@ -1,1 +1,28 @@
-export {}
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from './firebase';
+
+export function register(
+    email: string,
+    password: string,
+) {
+    return createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+    );
+}
+
+export function login(
+    email: string,
+    password: string,
+) {
+    return signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+    );
+}
+
+export function logout() {
+    return signOut(auth);
+}
